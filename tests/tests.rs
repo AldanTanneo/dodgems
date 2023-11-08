ade0021eb69e8207a8dd3cb6dce16bc1c8d381a3
César Sagaert <aldantanneo@gmail.com>
2023-11-08 13:40:19 +0100

    add tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,9 @@
 #![feature(allocator_api)]
 
-use std::mem::size_of;
+use std::{
+    alloc::{Allocator, Layout},
+    mem::size_of,
+};
 
 use dodgems::BumpCar;
 
@@ -53,4 +56,76 @@ fn allocate_failure() {
 
     let mut extra2: Vec<u8, _> = Vec::new_in(&b);
     assert!(matches!(extra2.try_reserve(128), Ok(())));
+}
+
+#[test]
+fn allocate_zero_size() {
+    let b = BumpCar::new(256).unwrap();
+
+    let _zero_size_alloc = (&b)
+        .allocate(Layout::from_size_align(0, 1).unwrap())
+        .unwrap();
+
+    assert_eq!(b.remaining_capacity(), 256);
+
+    let _fill_allocator = Vec::<u8, _>::with_capacity_in(256, &b);
+
+    assert_eq!(b.remaining_capacity(), 0);
+
+    let _zero_size_alloc = (&b)
+        .allocate(Layout::from_size_align(0, 1).unwrap())
+        .unwrap();
+
+    assert_eq!(b.remaining_capacity(), 0);
+}
+
+#[test]
+fn allocate_vary_alignment() {
+    let b = BumpCar::new(24).unwrap();
+
+    let _byte = Box::new_in(1i8, &b);
+    assert_eq!(b.remaining_capacity(), 23);
+
+    let _short = Box::new_in(2i16, &b);
+    assert_eq!(b.remaining_capacity(), 20);
+
+    // increase alignment, but add an extra byte to offset the current alignment
+    let _byte = Box::new_in(1i8, &b);
+    let _int = Box::new_in(4i32, &b);
+    assert_eq!(b.remaining_capacity(), 12);
+
+    let _byte = Box::new_in(1i8, &b);
+    let _long = Box::new_in(8i64, &b);
+    assert_eq!(b.remaining_capacity(), 0);
+}
+
+#[test]
+fn allocate_checkpoint() {
+    let b = BumpCar::new(256).unwrap();
+
+    let alloc_half = Vec::<u8, _>::with_capacity_in(128, &b);
+    drop(alloc_half);
+
+    assert_eq!(b.remaining_capacity(), 128);
+
+    let mut checkpoint = b.checkpoint();
+
+    assert_eq!(b.remaining_capacity(), 0);
+    assert_eq!(checkpoint.capacity(), 128);
+    assert_eq!(checkpoint.remaining_capacity(), 128);
+
+    let alloc_rest = Vec::<u8, _>::with_capacity_in(128, &checkpoint);
+    drop(alloc_rest);
+
+    assert_eq!(checkpoint.remaining_capacity(), 0);
+
+    let checkpoint2 = checkpoint.checkpoint();
+    assert_eq!(checkpoint2.capacity(), 0);
+    drop(checkpoint2);
+
+    checkpoint.reset();
+    assert_eq!(checkpoint.remaining_capacity(), 128);
+
+    drop(checkpoint);
+    drop(b);
 }
